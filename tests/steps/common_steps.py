94135fd073134f80e79bf6b3b042750d8d4d0217
ChristyBinu-4 <christy.binu91@gmail.com>
2026-02-02 13:27:07 +0530

    Deleted test files

--- tests/steps/common_steps.py
+++
@@ -1,2 +0,0 @@
-# Common step definitions shared across test files
-# Most steps have been moved to conftest.py to avoid duplication
--- tests/test_bdd_features.py
+++
@@ -1,8 +0,0 @@
-from pytest_bdd import scenarios
-import os
-
-# Use absolute path for features directory
-features_path = os.path.join(os.path.dirname(__file__), "features")
-scenarios(features_path)
-
-
